f3c00c963a2ae2d056d6a607857a87919d1a5b61
Naseer122 <k.naseer12@gmail.com>
2025-03-13 12:09:12 +0500

    Initial commit

---
+++ app/src/main/java/com/vpn/touchvpn/batch6firstapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.vpn.touchvpn.batch6firstapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.vpn.touchvpn.batch6firstapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
+    var Submit : Button? = null
+    //lateinit var Submit : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+
+
+
+    }
+
+}
